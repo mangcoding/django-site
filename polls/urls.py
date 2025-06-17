4bcf4590a4b4bbdd764f1f5f70d76fed88d6ad5b
nugrahaindie <nugraha@indietechsolutions.com>
2025-06-17 20:01:32 +0700

    add sample data to the databases

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("profile", views.profile, name="profile"),
+    path("contact", views.contact, name="contact"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = '<br>'.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def profile(request):
     return HttpResponse("Hello, I Write other text here. You're at the polls profile.")
+
+def contact(request):
+    return HttpResponse("Please contact me at 085759402332")
